92e9bc21281b0508b67840ff3065b7d07096bda1
extg <extg@yandex-team.ru>
2019-04-08 10:56:25 +0300

    Save image

--- src/index.js
+++ src/index.js
@@ -42,18 +42,22 @@ const Canvas = () => {
 const SaveButton = () => {
   const listener = {
     handleEvent(e) {
+      e.preventDefault()
       console.log(e)
       const canvas = document.getElementById('canvas')
       const image = canvas.toDataURL("image/jpg")
-      e.target.href = image
-      e.target.click()
+      // saveBase64AsFile(canvas)
+      saveBase64AsFile2(image)
+      // window.open(image)
+      // e.target.href = image
+      // e.target.click()
     },
     capture: true,
   };
 
   return html`
     <div>
-      <a @click=${listener} href="javascript:;">
+      <a class="link" @click=${listener}>
         Save
       </a> 
     </div>   
@@ -211,5 +215,24 @@ window.onload = () => {
   generateBasicHTML();
   generateCanvasContent();
 
-  saveButton.addEventListener("click", downloadImage);
+  // saveButton.addEventListener("click", downloadImage);
+}
+
+function saveBase64AsFile(canvas) {
+  var img = canvas.toDataURL()
+  var iframe = '<iframe src="' + img + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
+  var x = window.open();
+  x.document.open();
+  x.document.write(iframe);
+  x.document.close();
+}
+
+
+function saveBase64AsFile2(base64, fileName = 'collage.jpg') {
+
+  var link = document.createElement("a");
+
+  link.setAttribute("href", base64);
+  link.setAttribute("download", fileName);
+  link.click();
 }
